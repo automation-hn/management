d897ca50e02436b26cab0e5042345c75d318a183
nhatmnguyen211 <nhat.nm90@gmail.com>
2020-07-17 14:15:52 +0700

    Updates

---
+++ precache-manifest.02e82672ce11efd8a32a3436fa5c4e1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91308bc87f6ca60253dcffeb63f8e0a7",
+    "url": "/management/index.html"
+  },
+  {
+    "revision": "0f0bb3dce82a56dd9f68",
+    "url": "/management/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "bde6bc58908fd89de3ce",
+    "url": "/management/static/js/2.0b5c0282.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/management/static/js/2.0b5c0282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f0bb3dce82a56dd9f68",
+    "url": "/management/static/js/main.4d0bead8.chunk.js"
+  },
+  {
+    "revision": "f9124e1428ee5654a1e7",
+    "url": "/management/static/js/runtime-main.6f94a24c.js"
+  }
+]);
